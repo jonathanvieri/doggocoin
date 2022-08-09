9f235ee3b337963f4db6dd4b1009c5ce7ba9d5fb
jonathanvieri <jonathanvieri1321@gmail.com>
2022-08-09 20:41:49 +0700

    Change app icon and add constants file

---
+++ DoggoCoin/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  DoggoCoin
+//
+//  Created by Jonathan Vieri on 29/06/22.
+//
+
+import Foundation
+
+// C stands for Constants
+// This struct will contain all important variables that will later then be put into .gitignore
+struct C {
+    static let apiKey = "YOUR_API_KEY"
+    static let baseURL = "https://api.nomics.com/v1/currencies/ticker?ids=DOGE"
+}
